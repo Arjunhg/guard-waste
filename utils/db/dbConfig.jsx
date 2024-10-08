a94ed296d356a7ec540046a2b314dd47c8be22f2
Arjun Sharma <arjunsharma03.sa@gmail.com>
2024-10-08 16:36:31 +0530

    Guardian Check

--- utils/db/dbConfig.jsx
+++ utils/db/dbConfig.jsx
@@ -1,5 +1,3 @@
-// postgresql://neondb_owner:ZAzKQW1M7wnY@ep-lively-surf-a1jb4cgb.ap-southeast-1.aws.neon.tech/neondb?sslmode=require
-
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from './schema';
